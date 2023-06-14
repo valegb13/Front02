e714d139e97d7225693ffa6aa97b0a7d1b6b732c
Valen <vvisgal2@gmail.com>
2023-06-14 11:51:30 -0500

    instalación carousel

--- front02/src/app/app.module.ts
+++ front02/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
-
+import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,6 +21,8 @@ import { FormularioPagosComponent } from './Pages/formulario-pagos/formulario-pa
 import { ValidacionCarritoComponent } from './Pages/validacion-carrito/validacion-carrito.component';
 
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +38,9 @@ import { ValidacionCarritoComponent } from './Pages/validacion-carrito/validacio
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule,
+    CarouselModule
   ],
   providers: [CurrencyPipe],
   bootstrap: [AppComponent]
